0cf0f7f21aef01ad0f2fb7ffee02dc9cd9199064
nkamilov <001kamilov@gmail.com>
2022-12-19 11:06:04 +0600

    Update models"

--- store/items/models.py
+++ store/items/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class GlassesBuy(models.Model):
+    glasses_name = models.CharField(max_length=100)
+    glasses_type = models.CharField(max_length=100)
+    glasses_model_number = models.CharField(max_length=100)
+    glasses_country = models.CharField(max_length=100)
+    glasses_first_price = models.CharField(max_length=100)
+    glasses_buying_date = models.DateField()
+
+class GlassesSell(models.Model):
+    glasses = models.ForeignKey(GlassesBuy, on_delete=models.CASCADE)
+    glasses_type = models.CharField(max_length=100)
+    glasses_model_number = models.CharField(max_length=100)
+    glasses_country = models.CharField(max_length=100)
+    glasses_first_price = models.CharField(max_length=100)
+    glasses_count = models.IntegerField()
+
--- store/kamilov/settings.py
+++ store/kamilov/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'items',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'KZ'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
