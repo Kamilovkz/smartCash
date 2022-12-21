a4e7bcf8d0889bcf3deaaacbd387bce9d25ca9aa
nkamilov <001kamilov@gmail.com>
2022-12-22 01:11:02 +0600

    Update models for correct form

--- store/items/models.py
+++ store/items/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class GlassesBuy(models.Model):
-    glasses_name = models.CharField(max_length=100)
-    glasses_type = models.CharField(max_length=100)
-    glasses_model_number = models.CharField(max_length=100)
-    glasses_country = models.CharField(max_length=100)
-    glasses_first_price = models.CharField(max_length=100)
-    glasses_buying_date = models.DateField()
+class ItemStock(models.Model):
+    item_name = models.CharField(max_length=100)
+    item_type = models.CharField(max_length=100)
+    item_model_number = models.CharField(max_length=100)
+    item_country = models.CharField(max_length=100)
+    item_first_price = models.CharField(max_length=100)
+    item_buying_date = models.DateField()
+    item_count = models.IntegerField()
 
-class GlassesSell(models.Model):
-    glasses = models.ForeignKey(GlassesBuy, on_delete=models.CASCADE)
-    glasses_type = models.CharField(max_length=100)
-    glasses_model_number = models.CharField(max_length=100)
-    glasses_country = models.CharField(max_length=100)
-    glasses_first_price = models.CharField(max_length=100)
-    glasses_count = models.IntegerField()
+class ItemSell(models.Model):
+    name = models.ForeignKey(ItemStock, on_delete=models.CASCADE)
+    selling_date = models.DateField()
+    selling_price = models.CharField(max_length=100)
+    selling_count = models.IntegerField()
+    
+    
 
