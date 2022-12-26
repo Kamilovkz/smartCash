4a124bd12786cbbba7b51020a23d1b4655dfe6ed
Kamilovkz <001kamilov@gmail.com>
2022-12-26 22:51:02 +0600

    Update models to final

--- store/items/models.py
+++ store/items/models.py
@@ -1,20 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class ItemStock(models.Model):
+class Stock(models.Model):
     item_name = models.CharField(max_length=100)
     item_type = models.CharField(max_length=100)
     item_model_number = models.CharField(max_length=100)
     item_country = models.CharField(max_length=100)
-    item_first_price = models.CharField(max_length=100)
     item_buying_date = models.DateField()
-    item_count = models.IntegerField()
+    item_cost = models.CharField(max_length=100)
+    item_quantity = models.IntegerField(null=False, default=0)
 
-class ItemSell(models.Model):
-    name = models.ForeignKey(ItemStock, on_delete=models.CASCADE)
-    selling_date = models.DateField()
+class Product(models.Model):
+    name = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    selling_date = models.DateField(null=True, blank=True)
     selling_price = models.CharField(max_length=100)
-    selling_count = models.IntegerField()
-    
     
 
