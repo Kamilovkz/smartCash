3cf8ed5e94308bd50339c545d9e436ed0210671f
Kamilovkz <001kamilov@gmail.com>
2022-12-27 19:54:47 +0600

    add views

--- store/items/views.py
+++ store/items/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello Django. You are at the items index.")
 
+def detail(request, item_id):
+    return HttpResponse("You're looking at item %s" % item_id)
+
+def results(request, item_id):
+    response = "You're looking at the stock of items %s."
+    return HttpResponse(response % item_id)
+
+def vote(request, item_id):
+    return HttpResponse("You're chosing on item %s." % item_id)
