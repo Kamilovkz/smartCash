25238222fe6aa9e0ab31e2d690399a98ad53baaa
Kamilovkz <001kamilov@gmail.com>
2022-12-28 17:42:11 +0600

    Update views

--- store/items/views.py
+++ store/items/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Stock
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello Django. You are at the items index.")
+    latest_items_list = Stock.objects.order_by('-item_buying_date')[:5]
+    output = ', '.join([i.item_name for i in latest_items_list])
+    return HttpResponse(output)
 
 def detail(request, item_id):
     return HttpResponse("You're looking at item %s" % item_id)
