860df29075b0b4a90107a768c38224312dac2c39
Kamilovkz <001kamilov@gmail.com>
2023-01-04 02:15:45 +0600

    divided pages

--- store/items/urls.py
+++ store/items/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.GlassesListView.as_view(), name='index'),
+    path('', views.index, name='index'),
+    path('stock/', views.GlassesListView.as_view(), name='stock'),
     path('order/<int:pk>/', views.make_order, name='make_order'),
 ]
--- store/items/views.py
+++ store/items/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Glasses, Order
+from django.template import Context, loader
+from django.http import HttpResponse
 
 class GlassesListView(ListView):
     model = Glasses
     context_object_name = 'glasses'
-    template_name = 'items/index.html'
+    template_name = 'items/stock.html'
 
 def make_order(request, pk):
     glasses = Glasses.objects.get(pk=pk)
@@ -18,3 +20,9 @@ def make_order(request, pk):
         total_price=total_price
     )
     return render(request, 'items/order_confirmation.html', {'glasses': glasses, 'quantity': quantity, 'total_price': total_price})
+
+def stock(request):
+    return render(request, 'items/stock.html')
+
+def index(request):
+    return render(request, 'items/index.html')
