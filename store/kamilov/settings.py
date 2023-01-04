9f591db856a732b0d8170f9ef7d020016ab920c4
Kamilovkz <001kamilov@gmail.com>
2023-01-04 22:29:55 +0600

    Update readme and settings

--- store/kamilov/settings.py
+++ store/kamilov/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'en-en'
 
 TIME_ZONE = 'UTC'
 
