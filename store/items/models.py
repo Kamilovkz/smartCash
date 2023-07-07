cb3851995c770c723e7b8f7bb234e3b30fca9db1
Kamilovkz <001kamilov@gmail.com>
2023-07-07 19:19:23 +0000

    update project settings and tranfer to poetry

--- store/items/models.py
+++ store/items/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Glasses(models.Model):
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=255)
@@ -13,24 +14,23 @@ class Glasses(models.Model):
     def __str__(self):
         return self.name
 
+
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     glasses = models.ForeignKey(Glasses, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     total_price = models.DecimalField(max_digits=9, decimal_places=2)
     date_placed = models.DateField(auto_now_add=True)
-    discount = models.IntegerField(default='0')
-    status = models.CharField(max_length=255, default='confirmed')
+    discount = models.IntegerField(default=0)
+    status = models.CharField(max_length=255, default="confirmed")
 
     def __str__(self):
-        return f'Order {self.pk} by {self.user.username}'
-    
+        return f"Order {self.pk} by {self.user.username}"
+
     def calculate_total_price(self):
         self.total_price = self.glasses.price * self.quantity
-    
+
     def update_stock(self, inital_quantity):
         self.glasses.stock += inital_quantity
         self.glasses.stock -= self.quantity
         self.glasses.save()
-    
-    
--- store/items/scripts/reports.py
+++ store/items/scripts/reports.py
@@ -1,8 +1,8 @@
 from .config import render, model_to_dict
 from ..models import Order
 
+
 def reports(request):
     queryset = Order.objects.all().values()
-    context = {'data': queryset}
-    print(context)
-    return render(request, './items/reports.html', context)
+    context = {"data": queryset}
+    return render(request, "./items/reports.html", context)
